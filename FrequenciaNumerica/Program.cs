6a9e733b09b73bf1541a68d554843c7e14342473
User-Wellengton <wellengtonjm@gmail.com>
2022-02-18 16:26:03 -0300

    Add project files.

---
+++ FrequenciaNumerica/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace FrequenciaNumerica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+           
+
+            string opcao;
+            int tamanhoArray;
+            int contador = 0;
+            int numero;
+
+
+
+            Console.WriteLine("Digite o valor para o tamanho da array:");
+            tamanhoArray = Convert.ToInt32(Console.ReadLine());
+
+            int[] listaNumeros = new int [tamanhoArray];
+
+            for (int i = 0; i < tamanhoArray; i++)
+            {
+                Console.WriteLine("Digite o valor desejado:");
+
+                listaNumeros[i] = Convert.ToInt32(Console.ReadLine());
+
+
+            }
+
+
+            for (int i = 0; i < listaNumeros.Length; i++)
+            {
+                for (int j = i; j <listaNumeros.Length; j++)
+                {
+                    if ( i != j && listaNumeros[j] == listaNumeros[i])
+                    {
+                        contador++;
+
+                        numero = listaNumeros [j];  
+                    }
+
+                }   
+
+
+            }
+
+
+            Console.WriteLine(numero + "apareceu -> " + contador);
+
+            Console.ReadLine();
+        }
+    }
+}
